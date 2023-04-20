b71e11f7122b788a0211e1749626a6b376f41f60
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-04-20 15:55:41 -0700

    Centered div

--- src/components/dropbox.jsx
+++ src/components/dropbox.jsx
@@ -2,10 +2,11 @@ function Dropbox() {
   
     return (
       <div className ="w-[100%] flex flex-row justify-center">
-        <form>
+        <form className="">
             <label>
-                <input className="w-[688px] h-[224px]" type ="text" name = "inputText" placeholder = "Paste your text here..."/>
+                <textarea className="w-[688px] h-[224px]" type ="text" name = "inputText" placeholder = "Paste your text here..."/>
             </label>
+            
         </form>
       </div>
     )
