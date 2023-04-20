784e8895c1f9a19da1a4e16c47e80adce22d6140
jameskorrell <korrell@usc.edu>
2023-04-20 16:27:36 -0700

    added upload file

--- src/components/dropbox.jsx
+++ src/components/dropbox.jsx
@@ -1,12 +1,12 @@
 function Dropbox() {
   
     return (
-      <div className ="w-[100%] flex flex-row justify-center">
-        <form className="">
+      <div className ="w-[100%] flex justify-center">
+        <form className="" method = "post">
             <label>
                 <textarea className="w-[688px] h-[224px] placeholder:align-top resize-none" type ="text" name = "inputText" placeholder = "Paste your text here..."/>
             </label>
-            
+            <input className="block text-grey p[2px]" type="file" name="file"/>
         </form>
       </div>
     )
