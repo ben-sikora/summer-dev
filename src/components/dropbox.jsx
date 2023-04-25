7c44f49ae2e54cdc2c4a469e7871e93b93a7f6c5
jameskorrell <korrell@usc.edu>
2023-04-25 11:30:58 -0700

    small edits and changed port
    changed the const url to port 8000 to connect with backend, also changed the nav a little bit

--- src/components/dropbox.jsx
+++ src/components/dropbox.jsx
@@ -5,7 +5,7 @@ function Dropbox() {
 
 
   const onSubmit= () =>{
-    const url="https://523e0135-ec2c-4a77-b772-aaf375e95c18.mock.pstmn.io/text"; 
+    const url="http://127.0.0.1:8000/upload-text"; 
     fetch( url,{
       method: "POST",
       headers: {
@@ -23,7 +23,7 @@ function Dropbox() {
       <div className ="w-[100%] flex justify-center">
         <form className="" action='#0'>
             <label>
-                <textarea className="w-[688px] h-[224px] placeholder:align-top resize-none" type ="text" name = "inputText" placeholder = "Paste your text here..." onChange={(event)=>setText(event.target.value)}/>
+                <textarea className="w-[688px] h-[224px] placeholder:align-top resize-none rounded-md pl-1 pr-1" type ="text" name = "inputText" placeholder = "Paste your text here..." onChange={(event)=>setText(event.target.value)}/>
             </label>
             <input className="block text-grey p[2px]" type="file" name="file"/>
             <input
