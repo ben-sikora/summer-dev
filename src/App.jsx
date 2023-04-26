006d2ed166c0929c8bc96693fb32d0680feb5ff4
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-04-26 14:10:53 -0700

    Added gradients

--- src/App.jsx
+++ src/App.jsx
@@ -8,9 +8,13 @@ function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className='w-100% h-[150vh] bg-purple'>
-      <Nav />
-      <Dropbox />
+    <div className='w-[100%] h-[150vh] bg-purple grid grid-cols-1 grid-rows-1 overflow-clip'>
+      <div className='row-start-1 row-end-2 col-start-1 col-end-2 z-20'>
+        <Nav />
+        <Dropbox />
+      </div>
+      <div className='row-start-1 row-end-2 col-start-1 col-end-2 w-[100%] h-[150vh] bg-gradient-radial from-[#C5C5C5] opacity-25 z-0'> </div>
+      <div className='row-start-1 row-end-2 col-start-1 col-end-2 z-10 w-[80%] h-[800px] mx-auto bg-gradient-radial from-orange to-transparent to-70% mt-[110vh]'> </div>
     </div>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -16,6 +16,9 @@ export default {
 
 
     },
+    backgroundImage: {
+      'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+    }
   },
   },
   plugins: [],
