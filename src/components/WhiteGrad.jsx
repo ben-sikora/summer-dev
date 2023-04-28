6089bafcef295f6ad17b5c151b2d72b6713f50ba
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-04-27 20:11:14 -0700

    Small readability changes

--- src/components/WhiteGrad.jsx
+++ src/components/WhiteGrad.jsx
@@ -1,6 +1,6 @@
 function WhiteGrad(){
     return(
-    <div className="row-start-1 row-end-2 col-start-1 col-end-2 w-[100%] h-[150vh] bg-gradient-radial from-[#C5C5C5] opacity-25 z-0">
+    <div className="row-start-1 row-end-2 col-start-1 col-end-2 w-[100%] h-[100vh] bg-gradient-radial from-[#C5C5C5] opacity-25 z-0">
     </div>
     )
 }
--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -3,7 +3,7 @@ function Summary(props){
 
 
     return(
-        <div className="w-[688px] h-[224px] bg-white">
+        <div className="w-[688px] h-[224px] bg-white overflow-auto">
             <p>
                 {props.summaryText}
             </p>
