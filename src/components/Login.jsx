a2478789c8dce7d387cd07a3d8f1956610961ba1
jameskorrell <korrell@usc.edu>
2023-05-06 11:39:06 -0500

    idk what these changes are but i want ur pull

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,7 +1,7 @@
 function Login(){
 
     return(
-        <div className='box-border'>
+        <div className='box-border pt-[8px]'>
             <button className='bg-orange text-grey lg:h-[50px] md:h-[40px] h-[32px] lg:w-[160px] md:w-[128px] w-[100px] rounded-xl lg:text-[32px] mr-6 md:text-[26px] text-[18px]'> Login</button>
         </div>
     )
