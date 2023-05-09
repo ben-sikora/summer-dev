ac11f6c004b5f7a5e6fe91daa6f4a49422a1bc4c
jameskorrell <korrell@usc.edu>
2023-05-09 14:37:27 -0700

    second

--- src/components/Title.jsx
+++ src/components/Title.jsx
@@ -3,10 +3,10 @@ function Title(){
 
     return(
         <div>
-        <h1 className="font-title font-bold text-[16px] sm:text-[32px] md:text-[40px] lg:text-[64px] text-grey">
+        <h1 className="font-title font-bold text-[20px] sm:text-[32px] md:text-[46px] lg:text-[64px] text-grey">
               Welcome to Summer
             </h1>
-            <h2 className="font-title font-bold text-[12px] sm:text-[20px] md:text-[28px] lg:text-[40px] mt-[0px] sm:mt-[-20px] text-grey">
+            <h2 className="font-title font-bold text-[14px] sm:text-[24px] md:text-[32px] lg:text-[46px] mt-[0px] sm:mt-[-10px] text-grey">
               Summarize your documents...
         </h2>
         </div>
