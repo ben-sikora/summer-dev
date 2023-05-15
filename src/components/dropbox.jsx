0b8badb0341dfbe90b6c0909710aa71153490437
jameskorrell <korrell@usc.edu>
2023-05-15 12:37:34 -0700

    Update dropbox.jsx

--- src/components/dropbox.jsx
+++ src/components/dropbox.jsx
@@ -12,7 +12,7 @@ function Dropbox() {
   const spinner= '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>';
 
   const onSubmit= () =>{
-    const url="https://54.183.255.16:5001/upload-text"; 
+    const url="https://54.183.255.16:3000/upload-text"; 
     setSummaryStat('loading')
 
     if(!isFilePicked){
