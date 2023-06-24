22e0a8f79105c447d30b16cdf108db9c25cae12f
Ben Sikora <70484672+ben-sikora@users.noreply.github.com>
2023-06-24 11:19:12 -0700

    Adjust sizing of Summary Box

--- src/components/dropbox.jsx
+++ src/components/dropbox.jsx
@@ -6,15 +6,31 @@ function Dropbox() {
   const [text, setText] = useState("");
   const [summary, setSummary]= useState("");
   const [summaryStat, setSummaryStat]= useState("dorm");
-  const [selectedFile, setSelectedFile] = useState();
-	const [isFilePicked, setIsFilePicked] = useState(false);
+  //const [selectedFile, setSelectedFile] = useState();
+	//const [isFilePicked, setIsFilePicked] = useState(false);
 
   const spinner= '<div class="lds-ring"><div></div><div></div><div></div><div></div></div>';
 
   const onSubmit= () =>{
     const url="https://api-k4qp3cjfza-uc.a.run.app/upload-text"; 
     setSummaryStat('loading')
+    fetch( url, {
+      method: "POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        input: text
+      })
+    }
+    ).then(res =>res.json())
+    .then(data => {
+      setSummary(data.response)
+      setSummaryStat('arrived') 
+    }).catch(err => console.log(err));
 
+
+    /*
     if(!isFilePicked){
       fetch( url, {
         method: "POST",
@@ -49,31 +65,12 @@ function Dropbox() {
           setSummaryStat('arrived') 
         }).catch(err => console.log(err))
 
-      } 
+      }*/ 
 
       reader.onerror = function() {
         console.log(reader.error);
       };
-      
-    
-
-      /*
-      const formData = new FormData()
-      formData.append('file', selectedFile)
-      fetch( url,{
-        method: "POST",
-        headers:{
-
-        },
-        body: formData
-      }
-      ).then(res =>res.json())
-      .then(data => {
-        setSummary(data.response)
-        setSummaryStat('arrived') 
-      }).catch(console.log()); */
     }
-  }
   
     return (
       <div>
@@ -82,11 +79,12 @@ function Dropbox() {
             <label>
                 <textarea className="w-[100%] h-[224px] mt-[30px] placeholder:align-top resize-none rounded-md pl-1 pr-1" type ="text" name = "inputText" placeholder = "Paste your text here..." onChange={(event)=>setText(event.target.value)}/>
             </label>
+            {/* 
             <input className="block text-grey p-[2px]" type="file" name="file"
             onChange={(event)=> {
               setSelectedFile(event.target.files[0])
               setIsFilePicked(true)
-            }}/>
+            }}/> */}
             <input
               className="bg-orange text-grey text-[32px] w-[200px] h-[50px] rounded-xl"
               type="button"
--- src/components/summary.jsx
+++ src/components/summary.jsx
@@ -3,7 +3,7 @@ function Summary(props){
 
 
     return(
-        <div className="w-[688px] h-[224px] mt-[5px] bg-white overflow-auto rounded-md">
+        <div className="w-[200px] sm:w-[366px] md:w-[466px] lg:w-[688px] h-[224px] mt-4 bg-white overflow-auto rounded-md mb-5">
             <p>
                 {props.summaryText}
             </p>
